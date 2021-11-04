5533d08af28aa0a0f2d3c7dd87577ed5ef10f30a
shiquine <godchildshiquine@gmail.com>
2021-11-04 09:53:09 -0700

    py find

---
+++ finding.py
@@ -0,0 +1,7 @@
+var10 = 'hello world'
+print(var10.find('d'))
+# Find the placeholder of a string you would need to use the '.find' option.
+# you first need to declare a variable then assign it a value.
+
+var11 = 'hello world this is a nice day'
+print(var11.find('e')) # you can only find letters with this string
